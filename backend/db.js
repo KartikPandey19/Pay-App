ff227798a1fef73c01d68de112119ec4306e3c43
KartikPandey19 <kartik2000pandey@gmail.com>
2024-06-02 12:29:56 +0530

    Backend completed

---
+++ backend/db.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const {MONGODB_CONNECTION_STRING} = require("./config")
+
+mongoose.connect(MONGODB_CONNECTION_STRING)
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+})
+
+const accountSchema = mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+})
+
+const User = mongoose.model('User',userSchema);
+const Account = mongoose.model('Account',accountSchema);
+
+module.exports = {
+    User,
+    Account,
+}
---
+++ backend/transactionDb.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const {Account} = require("./db");
+
+const transferFunds = async (fromAccountId, toAccountId,amount) =>{
+    await Account.findByIdAndUpdate(fromAccountId,{$inc:{balance:-amount}} );
+
+    await Account.findByIdAndUpdate(toAccountId,{$inc:{balance:amount}} );
+
+}
+
+transferFunds('fromAccountId', 'toAccountId', 100);
